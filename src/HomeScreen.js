a61d639bad65172a8eb934bafc6a87841767b5e0
Abhishek <abhishek.singh@prismberry.com>
2025-12-29 11:28:26 +0530

    add dynamic color functionality

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,7 +1,7 @@
-import React from 'react'
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
 
 export default function HomeScreen({
+  buttonColor = '#2563eb',
   title = 'Home Screen',
   buttonText = 'Test SDK',
   onPress,
@@ -17,8 +17,7 @@ export default function HomeScreen({
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{title}</Text>
-
-      <TouchableOpacity style={styles.button} onPress={handlePress}>
+      <TouchableOpacity  style={[styles.button, { backgroundColor: buttonColor }]} onPress={handlePress}>
         <Text style={styles.buttonText}>{buttonText}</Text>
       </TouchableOpacity>
     </View>
@@ -39,7 +38,6 @@ const styles = StyleSheet.create({
     color: '#1f2937',
   },
   button: {
-    backgroundColor: '#2563eb',
     paddingVertical: 12,
     paddingHorizontal: 24,
     borderRadius: 8,
